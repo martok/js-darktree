3d406318254f15dda6ff7ec5668aefec1a5d0cb6
Martok <martok@martoks-place.de>
2022-11-19 22:45:58 +0100

    Add code

---
+++ src/darktree.js
@@ -0,0 +1,546 @@
+(function () {
+  "use strict";
+
+  // do install checks first
+  if (window.ShadowRoot || Element.prototype.attachShadow) {
+    if (!(window.ShadowRoot && Element.prototype.attachShadow)) {
+      throw new TypeError("DarkTree: ShadowRoot is partially implemented.");
+    }
+    // already standards compliant
+    return;
+  }
+
+  if (Node.prototype.getRootNode) {
+    // Ensure config dom.getRootNode.enabled is "false", or it would not work correctly
+    throw new TypeError("DarkTree: NodePrototype.getRootNode is available, but ShadowDOM is not.");
+  }
+
+  if (!customElements || !customElements.define) {
+    throw new TypeError("DarkTree: customElements registry not available.")
+  }
+
+  // save everything that will be changed later on
+
+  const propgetset = (o, p) => {
+    const { get, set } = Object.getOwnPropertyDescriptor(o, p);
+    return { get, set };
+  }
+
+  const Native = {
+    customElements: {
+      define: customElements.define.bind(customElements),
+    },
+    Element: {
+      children: propgetset(Element.prototype, "children"),
+      remove: Element.prototype.remove,
+    },
+    HTMLStyleElement: {
+    },
+    Node: {
+      appendChild: Node.prototype.appendChild,
+      childNodes: propgetset(Node.prototype, "childNodes"),
+      firstChild: propgetset(Node.prototype, "firstChild"),
+      insertBefore: Node.prototype.insertBefore,
+      lastChild: propgetset(Node.prototype, "lastChild"),
+      nextSibling: propgetset(Node.prototype, "nextSibling"),
+      parentNode: propgetset(Node.prototype, "parentNode"),
+      previousSibling: propgetset(Node.prototype, "previousSibling"),
+      removeChild: Node.prototype.removeChild,
+      textContent: propgetset(Node.prototype, "textContent"),
+    },
+  };
+
+  // Constants/Tokens
+
+  const ATTR_ID = "shadow-host-id";
+  const ATTR_SLOT_STATUS = "shadow-slot-status";
+
+  // Declarations
+
+  const allowedAttachElements = new Set(["article", "aside", "blockquote", "body", "div",
+                                         "footer", "h1", "h2", "h3", "h4", "h5", "h6",
+                                         "header", "main", "nav", "p", "section", "span"]);
+  let hostElementID = 1;
+
+  const NodeQ = new class NodeQ_ {
+    isShadowRoot(nodeLike) {
+      return nodeLike instanceof ShadowRoot;
+    }
+
+    getRoot(node) {
+      // return the enclosing ShadowRoot or the Document if not in a shadow tree
+      while (node.parentNode)
+        node = node.parentNode;
+      return node;
+    }
+
+    getShadowRoot(node) {
+      // return the enclosing ShadowRoot or null if not in a shadow tree
+      while (node.parentNode)
+        node = node.parentNode;
+      if (node instanceof ShadowRoot)
+        return node;
+      return null;
+    }
+
+    getShadowIncludingRoot(node) {
+      let root = getRoot(node);
+      while (isShadowRoot(root))
+        root = getRoot(root.host);
+      return root;
+    }
+  }
+
+  const Property = new class Property_ {
+    assignReadOnly(obj, name, value) {
+      Object.defineProperty(obj, name, { value, configurable: true });
+    }
+
+    mixin(target, template) {
+      for (const name of Object.getOwnPropertyNames(template)) {
+        Object.defineProperty(target, name, Object.getOwnPropertyDescriptor(template, name));
+      }
+    }
+
+    mro(obj, prop) {
+      if (!obj || obj == Object.prototype) {
+        return [];
+      }
+      const inherited = Property.mro(Object.getPrototypeOf(obj), prop);
+      const d = Object.getOwnPropertyDescriptor(obj, prop);
+      if (d) {
+        d._at = obj;
+        inherited.unshift(d);
+      }
+      return inherited;
+    }
+  }
+
+  const Style = new class Style_ {
+    constructor() {
+      this.css = null;
+      this.tempSheet = null;
+    }
+
+    ensureInstalled() {
+      if (this.css)
+        return;
+
+      this.css = document.createElement("style");
+      Native.Node.textContent.set.call(this.css,`
+        slot {
+          display: contents;
+        }
+      `);
+      Native.Node.appendChild.call(document.head, this.css);
+    }
+
+    parseCSS(source) {
+      if (!this.tempSheet) {
+        this.tempSheet = document.createElement("style");
+        Native.Node.appendChild.call(document.head, this.tempSheet);
+      }
+      Native.Node.textContent.set.call(this.tempSheet, source);
+      try {
+        return this.tempSheet.sheet.cssRules;
+      } finally {
+        Native.Node.textContent.set.call(this.tempSheet, "");
+      }
+    }
+  }
+
+  // Mixins and exported classes
+
+  class CERegistryMixin {
+    define(name, cls) {
+      allowedAttachElements.add(name);
+      try {
+        return Native.customElements.define.call(this, name, cls);
+      } catch(c) {
+          // rewrite JS exception classes from polyfill to correct DOMException
+          if (c instanceof SyntaxError) {
+            throw new DOMException(c.message, "SyntaxError");
+          } else
+          if (c instanceof Error && c.name==="Error") {
+            throw new DOMException(c.message, "NotSupportedError");
+          } else
+            throw c;
+      }
+    }
+  }
+
+  class ElementMixin {
+    attachShadow(init) {
+      if (this.shadowRoot !== undefined)
+        throw new DOMException(
+            `The <${this.tagName}> element has be tried to attach to is already a shadow host.`,
+            "InvalidStateError");
+      if (!allowedAttachElements.has(this.localName))
+        throw new DOMException(
+            `The <${this.tagName}> element does not supported to attach shadow`,
+            "NotSupportedError");
+      // ensure global CSS is installed
+      Style.ensureInstalled();
+      // set up the virtual root
+      let sr = new ShadowRoot();
+      Property.assignReadOnly(sr, "host", this);
+      Property.assignReadOnly(sr, "mode", init.mode);
+      Property.assignReadOnly(sr, "delegatesFocus", !!init.delegatesFocus);
+      sr.dtUnique = hostElementID++;
+      Property.assignReadOnly(this, "shadowRoot", init.mode === "closed" ? null : sr);
+      Property.assignReadOnly(this, "dtShadowRoot", sr);
+      // Enable Mixin DOM traversal code!
+      Property.assignReadOnly(this, "dtVirtualChildNodes", [... Native.Node.childNodes.get.call(this)]);
+      // give the ShadowRoot a new empty tree (rendering will reconstruct it from virtual)
+      Native.Node.textContent.set.call(this, "")
+      // force rendering once, which will at this point only detach unslotted bits from the tree
+      sr.dtRenderSync();
+      return sr;
+    }
+
+    get assignedSlot() {
+      const par = Native.Node.parentNode.get.call(this);
+      if (par && par instanceof HTMLSlotElement) {
+        return par;
+      }
+      return null;
+    }
+  }
+
+  class HTMLSlotElement extends HTMLElement {
+    constructor() {
+      super();
+    }
+
+    get name() {
+      return this.getAttribute("name") || "";
+    }
+    set name(val) {
+      if (val !== name) {
+        this.setAttribute("name", "val");
+        const sr = NodeQ.getShadowRoot(this);
+        if (sr) {
+          sr.dtNotifyNodeModified(sr);
+        }
+      }
+    }
+
+    dtUnslot() {
+      this.setAttribute(ATTR_SLOT_STATUS, "open");
+      // remove all slotted elements, and put back placeholder elements
+      // aka "realize" the dtVirtualChildNodes list
+      Native.Node.textContent.set.call(this, "");
+      for (const child of this.dtVirtualChildNodes) {
+        Native.Node.appendChild.call(this, child);
+      }
+    }
+
+    dtFillSlot(children) {
+      if (!children.length)
+        return;
+      this.setAttribute(ATTR_SLOT_STATUS, "filled");
+      // remove placeholder elements and add new children
+      Native.Node.textContent.set.call(this, "");
+      for (const child of children) {
+        Native.Node.appendChild.call(this, child);
+      }
+    }
+  }
+
+  class HTMLStyleElementMixin {
+    get textContent() {
+      if (this.dtOriginalTextContent)
+        return this.dtOriginalTextContent;
+      return Native.Node.textContent.get.call(this);
+    }
+
+    set textContent(text) {
+      delete this.dtOriginalTextContent;
+      Native.Node.textContent.set.call(this, text);
+      const sr = NodeQ.getShadowRoot(this);
+      if (sr)
+        sr.dtRenderSync();
+    }
+
+    dtUpdateGlobalized() {
+      const source = this.textContent;
+      const sr = NodeQ.getShadowRoot(this);
+      if (!sr) {
+        return;
+      }
+      // Save a copy of the original source before modifying
+      if (!this.dtOriginalTextContent) {
+        this.dtOriginalTextContent = source;
+      }
+      const selfSelector = sr.dtHostSelector;
+      // 1. translate :host(-content) pseudoselector first, because it would be a parser error
+      const rules = Style.parseCSS(source.replace(
+                                    // flag "s" is broken in SeaMonkey
+                                    /:host(-context)?(?:\(([\s\S]+?)\))?/g,
+                                    function ($, context, selectors) {
+                                      return !context ? !selectors ? selfSelector :
+                                          `${selfSelector}:-moz-any(${selectors})` :
+                                          `:-moz-any(${selectors}) ${selfSelector}`;
+                                    }));
+      // 2. prefix all rules that were not :host-relative already with the host child selector and transfer back to local style
+      const newRules = [];
+      for (const rule of rules) {
+        const css = (rule instanceof CSSStyleRule) && !rule.selectorText.includes(selfSelector) ?
+                    selfSelector + " " + rule.cssText :
+                    rule.cssText;
+        newRules.push(css);
+      }
+      // 3. assign modified sheet
+      Native.Node.textContent.set.call(this, newRules.join("\n"));
+    }
+  }
+
+  class NodeMixin {
+    // Polyfill
+    getRootNode(opt) {
+      let composed = typeof opt === "object" && !!(opt.composed);
+      return composed ? getShadowIncludingRoot(this) : getRoot(this);
+    }
+
+    get childNodes() {
+      if (this.dtVirtualChildNodes) {
+        return [... this.dtVirtualChildNodes];
+      }
+      return Native.Node.childNodes.get.call(this);
+    }
+
+    dtClearChildNodes() {
+      if (this.dtVirtualChildNodes) {
+        for (const node of this.dtVirtualChildNodes) {
+          const par = Native.Node.parentNode.get.call(node);
+          if (par) {
+            Native.Node.removeChild.call(par, node);
+          }
+          delete node.dtVirtualParent;
+        }
+        this.dtVirtualChildNodes.splice(0, this.dtVirtualChildNodes.length);
+        if (this.dtShadowRoot)
+          this.dtShadowRoot.dtRenderSync();
+      } else {
+        Native.Node.textContent.set.call(this, "");
+      }
+    }
+
+    get parentNode() {
+      // i.e. slotted element -> parent is the host
+      if (this.dtVirtualParent)
+        return this.dtVirtualParent;
+      const parent = Native.Node.parentNode.get.call(this);
+      if (!parent) {
+        return parent;
+      }
+      if (parent.dtVirtualChildNodes) {
+        // parent has virtual children. is this one of them? -> "true" parent
+        if (parent.dtVirtualChildNodes.includes(this))
+          return parent;
+        // no. is the parent a shadow host?
+        if (parent.dtShadowRoot)
+          return parent.dtShadowRoot;
+        // no. this node is virtually detached
+        return null;
+      }
+      // regular parent element
+      return parent;
+    }
+
+    appendChild(child) {
+      return this.insertBefore(child, null);
+    }
+
+    insertBefore(child, reference) {
+      if (child)
+        delete child.dtVirtualParent;
+      if (this.dtVirtualChildNodes) {
+        const idx = reference ?
+                      this.dtVirtualChildNodes.findIndex((e) => e === reference) :
+                      this.dtVirtualChildNodes.length;
+        if (idx < 0)
+          throw new DOMException("Node was not found", "NotFoundError");
+        const insert = child instanceof DocumentFragment ? [... child.childNodes] : [child];
+        Array.splice.apply(this.dtVirtualChildNodes, [idx, 0, ...insert]);
+        Property.assignReadOnly(child, "dtVirtualParent", this);
+        if (this.dtShadowRoot) {
+          // either slot it or set it aside
+          this.dtShadowRoot.dtRenderSync();
+        }
+        return child;
+      }
+      return Native.Node.appendChild.call(this, child);
+    }
+
+    removeChild(child) {
+      if (child)
+        delete child.dtVirtualParent;
+      if (this.dtVirtualChildNodes && child) {
+        const idx = this.dtVirtualChildNodes.findIndex((e) => e === child);
+        if (idx < 0)
+          throw new DOMException("Node was not found", "NotFoundError");
+        this.dtVirtualChildNodes.splice(idx, 1);
+        if (this.dtShadowRoot)
+          this.dtShadowRoot.dtRenderSync();
+        return child;
+      }
+      return Native.Node.removeChild.call(this, child);
+    }
+
+    get textContent() {
+      if (this.dtVirtualChildNodes) {
+        const parts = [];
+        for (const node of this.dtVirtualChildNodes) {
+          parts.push(node.textContent);
+        }
+        return parts.join("");
+      }
+      return Native.Node.textContent.get.call(this);
+    }
+    set textContent(text) {
+      if (this.dtVirtualChildNodes) {
+        this.dtClearChildNodes();
+        const tn = document.createTextNode(text);
+        this.appendChild(tn);
+      } else {
+        Native.Node.textContent.set.call(this, text);
+      }
+    }
+  }
+
+  class ShadowRoot extends DocumentFragment {
+    // host: Element
+    // mode: str
+    // delegatesFocus: bool
+
+    appendChild(child) {
+      return this.insertBefore(child, null);
+    }
+
+    insertBefore(child, reference) {
+      const ret = Native.Node.insertBefore.call(this.host, child, reference);
+      this.dtRenderSync();
+      return ret;
+    }
+
+    get firstChild() {
+      let node = Native.Node.firstChild.get.call(this.host);
+      while (node && this.host.dtVirtualChildNodes.includes(node)) {
+        node = Native.Node.nextSibling.get.call(node);
+      }
+      return node;
+    }
+
+    get lastChild() {
+      let node = Native.Node.lastChild.get.call(this.host);
+      while (node && this.host.dtVirtualChildNodes.includes(node)) {
+        node = Native.Node.previousSibling.get.call(node);
+      }
+      return node;
+    }
+
+    get dtUnique() {
+      return this.host.getAttribute(ATTR_ID);
+    }
+    set dtUnique(id) {
+      this.host.setAttribute(ATTR_ID, id);
+    }
+
+    get dtHostSelector() {
+      return `${this.host.localName}[${ATTR_ID}="${this.dtUnique}"]`;
+    }
+
+    dtEnsureSlotClasses() {
+      const allSlots = [];
+      const recurse = (parent) => {
+        let node = parent.firstChild;
+        while (node) {
+          // upgrade node if necessary
+          if (node.nodeType === Node.ELEMENT_NODE && node.localName === "slot" && !HTMLSlotElement.prototype.isPrototypeOf(node)) {
+            Object.setPrototypeOf(node, HTMLSlotElement.prototype);
+            // if we *just* did that, nothing can have been previously slotted. so, the current content is the fallback content
+            Property.assignReadOnly(node, "dtVirtualChildNodes", [... Native.Node.childNodes.get.call(node)]);
+          }
+          if (node instanceof HTMLSlotElement) {
+            allSlots.push(node);
+          } else {
+            recurse(node);
+          }
+          node = node.nextSibling;
+        }
+      }
+      recurse(this);
+      return allSlots;
+    }
+
+    dtRenderSync() {
+      const selfHost = this.host;
+      // prepare slots and slotted elements
+      const allSlots = this.dtEnsureSlotClasses();
+      let nodesToSlot = [... selfHost.dtVirtualChildNodes];
+      // unslot everything
+      const pastAndPresentSlots = new Set(allSlots);
+      for (const n of nodesToSlot) {
+        const slot = n.assignedSlot;
+        if (slot)
+          pastAndPresentSlots.add(slot);
+      }
+      for (const slot of pastAndPresentSlots) {
+        slot.dtUnslot();
+      }
+      // go through the dark tree and assemble what we have
+      recursiveRender(this);
+      // any nodesToSlot that wasn't slotted remains detached
+
+      function recursiveRender(parent) {
+        let node = parent.firstChild;
+        while (node) {
+          if (node instanceof HTMLSlotElement) {
+            slotFill(node);
+          } else if (node instanceof HTMLStyleElement) {
+            node.dtUpdateGlobalized();
+          } else {
+            recursiveRender(node);
+          }
+          node = node.nextSibling;
+        }
+      }
+
+      function slotFill(slot) {
+        const slotName = slot.name;
+        const fnTest = slotName ?
+                         // named slots accept all elements that have the corresponding slot name
+                         (node) => node.nodeType == Node.ELEMENT_NODE && node.getAttribute("slot") === slotName :
+                         // An unnamed <slot> will be filled with all of the custom element's top-level child nodes that do not have the slot attribute. This includes text nodes.
+                         (node) => node.nodeType !== Node.ELEMENT_NODE || !node.hasAttribute("slot");
+        const matched = nodesToSlot.filter((node) => fnTest(node));
+        if (!!matched.length) {
+          slot.dtFillSlot(matched);
+          nodesToSlot = nodesToSlot.filter((node) => !matched.includes(node));
+          return true;
+        }
+        return false;
+      }
+    }
+  }
+
+  // installation
+
+  function installExports() {
+    window.__DT_Native = Native;
+    window.__DT_Property = Property;
+    window.ShadowRoot = ShadowRoot;
+    window.HTMLSlotElement = HTMLSlotElement;
+  }
+
+  function installPatches() {
+    Property.mixin(customElements, CERegistryMixin.prototype);
+    Property.mixin(Element.prototype, ElementMixin.prototype);
+    Property.mixin(HTMLStyleElement.prototype, HTMLStyleElementMixin.prototype);
+    Property.mixin(Node.prototype, NodeMixin.prototype);
+  }
+
+  installExports();
+  installPatches();
+}())
