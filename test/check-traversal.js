ad248f7335062f4e2e6eb631b00221cac8bb23f9
Martok <martok@martoks-place.de>
2022-11-20 18:10:48 +0100

    test: DOM tree visualisation

---
+++ test/check-traversal.js
@@ -0,0 +1,145 @@
+(function () {
+  "use strict";
+
+
+  function printNode(aNode, aTarget) {
+    const doc = aTarget.ownerDocument;
+
+    const item = doc.createElement("div");
+    item.className = "node";
+    aTarget.appendChild(item);
+
+    switch(aNode.nodeType) {
+      case Node.ELEMENT_NODE:
+        item.classList.add("element");
+        item.innerHTML = `<span class="tagName">${aNode.localName}<span/>`;
+        for (const attr of aNode.attributes) {
+          const span = doc.createElement("span");
+          span.className = "attribute";
+          span.innerHTML = `<span class="attrName"></span>=<span class="attrValue"></span>`;
+          span.firstChild.textContent = attr.name;
+          span.lastChild.textContent = attr.value;
+          item.firstChild.appendChild(span);
+        }
+        break;
+      case Node.TEXT_NODE:
+        item.classList.add("text");
+        let text = aNode.textContent;
+        if (aNode.parentNode.localName == "style") {
+          text = aNode.parentNode.textContent;
+        }
+        if (/^\s+$/.test(text)) {
+          text = text.replaceAll("\n", "\u00b6").replaceAll(" ", "\u00b7");
+        }
+        item.textContent = text;
+        break;
+      case Node.DOCUMENT_FRAGMENT_NODE:
+        item.classList.add("document");
+        item.textContent = aNode.host ? "#shadow-root" : aNode.nodeName;
+        break;
+      case Node.COMMENT_NODE:
+        item.classList.add("comment");
+        item.textContent = aNode.textContent;
+        break;
+      default:
+        item.textContent = aNode.nodeName;
+    }
+
+    const childList = doc.createElement("ul");
+
+    if (aNode.dtShadowRoot) {
+      const item = doc.createElement("li");
+      childList.appendChild(item);
+      printNode(aNode.dtShadowRoot, item);
+    }
+
+    let child = aNode.firstChild;
+    while (child) {
+      const item = doc.createElement("li");
+      childList.appendChild(item);
+      printNode(child, item);
+      child = child.nextSibling;
+    }
+    if (childList.childElementCount>0) {
+      aTarget.appendChild(childList);
+    }
+  }
+
+  function checkTraversal(node) {
+    const outputWindow = window.open("", "__DT_checkTraversal", "popup=yes,width=500,height=400");
+    const outputDoc = outputWindow.document;
+    outputDoc.open(); outputDoc.close();
+    const head = outputDoc.head;
+    const body = outputDoc.body;
+
+    const style = outputDoc.createElement("style");
+    style.textContent = `
+      body {
+        font-family: sans-serif;
+        font-size: 10pt;
+      }
+      ul {
+        padding-left: 1em;
+        margin: 0;
+      }
+
+      ul li {
+        list-style-type: none;
+        border-left: 1px solid lightgray;
+        padding-left: 5px;
+      }
+
+      .node:hover {
+        background-color: lavender;
+      }
+      .node.text {
+        white-space: pre-line;
+      }
+      .node.document {
+        color: green;
+      }
+      .node.comment {
+        color: gray;
+        font-style: italic;
+      }
+      .node.comment::before {
+        content: "<!-- ";
+      }
+      .node.comment::after {
+        content: "-->";
+      }
+      .tagName {
+        color: blue;
+      }
+      .tagName::before {
+        content: "<";
+        color: #000;
+      }
+      .tagName::after {
+        content: ">";
+        color: #000;
+      }
+      .tagName .attrName {
+        color:#dc4866;
+      }
+      .tagName .attrName::before {
+        content: " ";
+      }
+      .tagName .attrValue {
+        color:#48b1dc;
+      }
+      .tagName .attrValue::before,
+      .tagName .attrValue::after {
+        content: '"';
+        color: #000;
+      }
+    `;
+    head.appendChild(style);
+
+    const tree = outputDoc.createElement("div");
+    body.appendChild(tree);
+    printNode(node, tree);
+  }
+
+  window.__DT_checkTraversal = checkTraversal;
+}())
